f1dcb2f9e4708acf1649a2cda8b6c3da8e04c17c
Raphael Borges <rah-borges@hotmail.com>
2020-08-24 18:16:28 -0300

    build(configs):rm antiga main, incluido boot configs

--- src/main/java/org/com/javasheets/demo/SpringBootJspApplication.java
+++ src/main/java/org/com/javasheets/demo/SpringBootJspApplication.java
@@ -1,13 +1,20 @@
 package org.com.javasheets.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+@SpringBootApplication
+public class SpringBootJspApplication extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DemoApplication.class);
+		return application.sources(SpringBootJspApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJspApplication.class, args);
 	}
 
 }
