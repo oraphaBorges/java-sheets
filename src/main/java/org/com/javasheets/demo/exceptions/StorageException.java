a50de138c5d014710c976f1f06eab41c679661b4
Raphael Borges <rah-borges@hotmail.com>
2020-08-26 16:57:49 -0300

    refactor(service e exception):rm serviço e exceção não usadas

---
+++ src/main/java/org/com/javasheets/demo/exceptions/StorageException.java
@@ -0,0 +1,12 @@
+package org.com.javasheets.demo.exceptions;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
