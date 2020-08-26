648f5832cb8c37afaacd4cfd20e039979ec06adf
Raphael Borges <rah-borges@hotmail.com>
2020-08-26 15:22:50 -0300

    reactor(upload file):tratando os arquivo com o opencsv

--- src/main/java/org/com/javasheets/demo/controllers/IndexController.java
+++ src/main/java/org/com/javasheets/demo/controllers/IndexController.java
@@ -1,13 +1,23 @@
 package org.com.javasheets.demo.controllers;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+
+import org.com.javasheets.demo.models.User;
 import org.com.javasheets.demo.services.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 
 @Controller
 public class IndexController{
@@ -21,10 +31,38 @@ public class IndexController{
 	}
 	
     @PostMapping(value="/upload",consumes = {"multipart/form-data"})
-    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes,Model model) {
 
-        fileService.uploadFile(file);
+        // validate file
+        if (file.isEmpty()) {
+            model.addAttribute("message", "Please select a CSV file to upload.");
+            model.addAttribute("status", false);
+        } else {
 
-        return "redirect:/";
+            // parse CSV file to create a list of `User` objects
+            try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+
+                // create csv bean reader
+                CsvToBean<User> csvToBean = new CsvToBeanBuilder(reader)
+                        .withType(User.class)
+                        .withIgnoreLeadingWhiteSpace(true)
+                        .build();
+
+                // convert `CsvToBean` object to list of users
+                List<User> users = csvToBean.parse();
+
+                // TODO: save users in DB?
+
+                // save users list on model
+                model.addAttribute("users", users);
+                model.addAttribute("status", true);
+
+            } catch (Exception ex) {
+                model.addAttribute("message", "An error occurred while processing the CSV file.");
+                model.addAttribute("status", false);
+            }
+        }
+
+        return "file-upload-status";
     }
 }
