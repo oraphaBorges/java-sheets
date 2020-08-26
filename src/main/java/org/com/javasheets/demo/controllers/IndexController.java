c18ed030d11fad5edd4489d7356c67580b21b836
Raphael Borges <rah-borges@hotmail.com>
2020-08-26 16:58:27 -0300

    refactor(common-csv-apache):adaptado para nova lib

--- src/main/java/org/com/javasheets/demo/controllers/IndexController.java
+++ src/main/java/org/com/javasheets/demo/controllers/IndexController.java
@@ -3,10 +3,13 @@ package org.com.javasheets.demo.controllers;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.com.javasheets.demo.models.User;
-import org.com.javasheets.demo.services.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,14 +19,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
 
 @Controller
 public class IndexController{
-	
-	@Autowired
-	FileService fileService;
 	
 	@GetMapping("/")
 	public String index() {
@@ -33,36 +31,47 @@ public class IndexController{
     @PostMapping(value="/upload",consumes = {"multipart/form-data"})
     public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes,Model model) {
 
-        // validate file
         if (file.isEmpty()) {
             model.addAttribute("message", "Please select a CSV file to upload.");
             model.addAttribute("status", false);
         } else {
 
-            // parse CSV file to create a list of `User` objects
-            try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            try {
+            	// Leitor do arquivo recebido da requisição POST
+            	Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
 
-                // create csv bean reader
-                CsvToBean<User> csvToBean = new CsvToBeanBuilder(reader)
-                        .withType(User.class)
-                        .withIgnoreLeadingWhiteSpace(true)
-                        .build();
+            	// Criado o Objeto CSVFormat
+            	// Que define os delimitador entre os itens de cada linha
+        		CSVFormat format = CSVFormat.RFC4180.withHeader().withDelimiter(',');
+        		CSVParser parser = new CSVParser(reader, format);
 
-                // convert `CsvToBean` object to list of users
-                List<User> users = csvToBean.parse();
+        		// Percorrendo os arquivos da lista de acordo com o Objeto desejado
+        		// e os identificadores do CSV
+        		List<User> users = new ArrayList<User>();
+        		for(CSVRecord record : parser){
+        			User user = new User();
+        			user.setId(Long.parseLong(record.get("id")));
+        			user.setName(record.get("name"));
+        			user.setEmail(record.get("email"));
+        			user.setCountryCode(record.get("country"));
+        			user.setAge(Integer.parseInt(record.get("age")));
+        			users.add(user);
+        		}
 
-                // TODO: save users in DB?
+        		// fechando o parser
+        		parser.close();
 
-                // save users list on model
-                model.addAttribute("users", users);
-                model.addAttribute("status", true);
+                // Passando os objetos para serem enviados na requisão para a página de listagem
+        		model.addAttribute("status", true);
+        		model.addAttribute("users", users);
 
             } catch (Exception ex) {
                 model.addAttribute("message", "An error occurred while processing the CSV file.");
                 model.addAttribute("status", false);
             }
         }
-
+        
+        
         return "file-upload-status";
     }
 }
